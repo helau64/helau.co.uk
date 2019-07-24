8e595f0f7236cdfa5a45741624c53ee7e8b46bad
Heather <hello@heatherlauren.me>
2019-07-24 15:53:27 +0100

    ?

--- src/cms/preview-templates/PortfolioItemPreview.js
+++ src/cms/preview-templates/PortfolioItemPreview.js
@@ -3,13 +3,16 @@ import PropTypes from 'prop-types'
 import { PortfolioItemTemplate } from '../../templates/portfolio-item'
 
 const PortfolioItemPreview = ({ entry, widgetFor }) => {
+  const entryImages = entry.getIn(['data', 'images'])
+  const images = entryImages ? entryImages.toJS() : []
+
   return (
     <PortfolioItemTemplate
       content={widgetFor('body')}
       tags={entry.getIn(['data', 'tags'])}
       title={entry.getIn(['data', 'title'])}
       date={entry.getIn(['data', 'date'])}
-      images={entry.getIn(['data', 'images'])}
+      images={{ images }}
     />
   )
 }
