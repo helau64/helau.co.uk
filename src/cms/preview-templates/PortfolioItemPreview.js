5d6922f7886ce5dfad7d999fae39389af823a70c
Heather <hello@heatherlauren.me>
2019-07-24 16:09:15 +0100

    ?

--- src/cms/preview-templates/PortfolioItemPreview.js
+++ src/cms/preview-templates/PortfolioItemPreview.js
@@ -2,24 +2,20 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { PortfolioItemTemplate } from '../../templates/portfolio-item'
 
-const PortfolioItemPreview = ({ entry, widgetFor }) => {
-  const entryImages = entry.getIn(['data', 'images'])
-  const images = entryImages ? entryImages.toJS() : []
-  return (
-    <PortfolioItemTemplate
-      content={widgetFor('body')}
-      tags={entry.getIn(['data', 'tags'])}
-      title={entry.getIn(['data', 'title'])}
-    />
-  )
-}
+const PortfolioItemPreview = ({ entry, widgetFor }) => (
+  <PortfolioItemTemplate
+    content={widgetFor('body')}
+    description={entry.getIn(['data', 'description'])}
+    tags={entry.getIn(['data', 'tags'])}
+    title={entry.getIn(['data', 'title'])}
+  />
+)
 
 PortfolioItemPreview.propTypes = {
   entry: PropTypes.shape({
     getIn: PropTypes.func,
   }),
-  widgetFor: PropTypes.func
-
+  widgetFor: PropTypes.func,
 }
 
 export default PortfolioItemPreview
